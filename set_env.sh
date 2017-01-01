f03241371149730fe41adea1734502ff9b17c718
Petraea <g.j.s.dawes@gmail.com>
2017-01-01 01:46:25 +0100

    Add submodules.

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
-cd ../esp-idf
+cd esp-idf
 export IDF_PATH=`pwd`
 source add_path.sh
 cd -
 
-cd ../xtensa-esp32-elf/bin/
+cd nodemcu-prebuilt-toolchains/esp32/xtensa-esp32-elf/bin/
 export PATH=$PATH:`pwd`
 cd -
