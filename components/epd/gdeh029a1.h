b87ada6689ca9d5dcbfa96ea224f4e78ecb71f7e
Bas van Sisseren <bas@vansisseren.nl>
2017-01-12 21:34:08 +0100

    add more LUT waveforms;add underlined text support

--- components/epd/gdeh029a1.h
+++ components/epd/gdeh029a1.h
@@ -10,15 +10,23 @@
 
 #define DISP_SIZE_X_B ((DISP_SIZE_X + 7) >> 3)
 
-extern void initDisplay(bool fast);
-extern void writeLUT(bool fast);
+#define LUT_DEFAULT 0
+#define LUT_FULL    0
+#define LUT_PART    1
+#define LUT_FASTER  2
+#define LUT_FASTEST 3
+#define LUT_MAX     3
+extern void initDisplay(int lut_idx);
+extern void writeLUT(int lut_idx);
 
 extern void drawImage(const unsigned char *picture);
 
-#define FONT_16PX       0x01
-#define FONT_INVERT     0x02
-#define FONT_FULL_WIDTH 0x04
-#define FONT_MONOSPACE  0x08
+#define FONT_16PX        0x01
+#define FONT_INVERT      0x02
+#define FONT_FULL_WIDTH  0x04
+#define FONT_MONOSPACE   0x08
+#define FONT_UNDERLINE_1 0x10
+#define FONT_UNDERLINE_2 0x20
 extern int drawText(int x, int y, int y_len, const char *text, uint8_t flags);
 
 extern void setRamArea(unsigned char Xstart, unsigned char Xend,
