d6ae236b6286e7bec52d59c625e8a692453547cc
Bas van Sisseren <bas@vansisseren.nl>
2017-01-21 20:01:25 +0100

    add picture from OHM2013.

---
+++ main/img_hacking.h
@@ -0,0 +1,18 @@
+#ifndef IMG_HACKING
+#define IMG_HACKING
+
+#include <stdint.h>
+
+/*
+ * original image found on https://www.flickr.com/photos/adulau/9464930917/
+ * (Alexandre Dulaunoy)
+ *
+ * "Feel free to reuse, use and abuse my pictures under the CC-SA license,
+ *  the GNU Free Documentation License or the GNU General Public License."
+ *
+ * image is cropped and resized to 296x128 pixels.
+ */
+
+extern const uint8_t img_hacking[37888];
+
+#endif // IMG_HACKING
