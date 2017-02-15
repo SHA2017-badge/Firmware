22dd7646d5c62ffe4c22f8d9c0ede18f3d2183ed
Thomas Roos <thomas@GLaDOS>
2017-02-15 17:00:51 +0100

    This time also the pins file :)

--- components/epd/pins.h
+++ components/epd/pins.h
@@ -1,11 +1,11 @@
 #ifndef EPD_PINS_H
 #define EPD_PINS_H
 
-#define PIN_NUM_CLK 5
-#define PIN_NUM_MOSI 17
-#define PIN_NUM_CS 18
-#define PIN_NUM_DATA 19
+
+#define PIN_NUM_MOSI 5
+#define PIN_NUM_CS 19
+#define PIN_NUM_DATA 21
 #define PIN_NUM_RESET 23  // hardware: patch 20 to 23
-#define PIN_NUM_BUSY 21
+#define PIN_NUM_BUSY 22
 
 #endif
