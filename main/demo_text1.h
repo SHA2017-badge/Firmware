f71d4d0e6f3661dd39a692cb410683eef36f2a5b
Bas van Sisseren <bas@vansisseren.nl>
2017-02-18 17:48:44 +0100

    add '#ifndef X; #define X; ...; #endif // X' to header files

--- main/demo_text1.h
+++ main/demo_text1.h
@@ -1,1 +1,6 @@
+#ifndef DEMO_TEXT1_H
+#define DEMO_TEXT1_H
+
 extern void demoText1(void);
+
+#endif // DEMO_TEXT1_H
--- main/demo_text2.h
+++ main/demo_text2.h
@@ -1,1 +1,6 @@
+#ifndef DEMO_TEXT2_H
+#define DEMO_TEXT2_H
+
 extern void demoText2(void);
+
+#endif // DEMO_TEXT2_H
