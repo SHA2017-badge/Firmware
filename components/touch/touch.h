84ed2504d5d676c2f6fbea9685a7c7763d74aad6
Anne Jan Brouwer <brouwer@annejan.com>
2017-02-18 16:50:08 +0100

    Moved code to touch.c

--- components/touch/touch.h
+++ components/touch/touch.h
@@ -1,6 +1,8 @@
 #ifndef TOUCH_H
 #define TOUCH_H
 
+#include "driver/i2c.h"
+
 #define I2C_MASTER_SCL_IO 27        /*!<gpio number for i2c slave clock  */
 #define I2C_MASTER_SDA_IO 26        /*!<gpio number for i2c slave data */
 #define I2C_MASTER_NUM I2C_NUM_1    /*!< I2C port number for master dev */
@@ -16,74 +18,6 @@
 #define ACK_VAL 0x0       /*!< I2C ack value */
 #define NACK_VAL 0x1      /*!< I2C nack value */
 
-/**
- * @brief test code to read esp-i2c-slave
- *        We need to fill the buffer of esp slave device, then master can read
- * them out.
- *
- * _______________________________________________________________________________________
- * | start | slave_addr + rd_bit +ack | read n-1 bytes + ack | read 1 byte +
- * nack | stop |
- * --------|--------------------------|----------------------|--------------------|------|
- *
- */
-esp_err_t i2c_master_read_slave(i2c_port_t i2c_num, uint8_t *data_rd,
-                                size_t size) {
-  if (size == 0) {
-    return ESP_OK;
-  }
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, (ESP_SLAVE_ADDR << 1) | READ_BIT, ACK_CHECK_EN);
-  if (size > 1) {
-    i2c_master_read(cmd, data_rd, size - 1, ACK_VAL);
-  }
-  i2c_master_read_byte(cmd, data_rd + size - 1, NACK_VAL);
-  i2c_master_stop(cmd);
-  esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
-  i2c_cmd_link_delete(cmd);
-  return ret;
-}
-
-/**
- * @brief Test code to write esp-i2c-slave
- *        Master device write data to slave(both esp32),
- *        the data will be stored in slave buffer.
- *        We can read them out from slave buffer.
- *
- * ___________________________________________________________________
- * | start | slave_addr + wr_bit + ack | write n bytes + ack  | stop |
- * --------|---------------------------|----------------------|------|
- *
- */
-esp_err_t i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr,
-                                 size_t size) {
-  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-  i2c_master_start(cmd);
-  i2c_master_write_byte(cmd, (ESP_SLAVE_ADDR << 1) | WRITE_BIT, ACK_CHECK_EN);
-  i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
-  i2c_master_stop(cmd);
-  esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
-  i2c_cmd_link_delete(cmd);
-  return ret;
-}
-
-/**
- * @brief i2c master initialization
- */
-void i2c_master_init() {
-  int i2c_master_port = I2C_MASTER_NUM;
-  i2c_config_t conf;
-  conf.mode = I2C_MODE_MASTER;
-  conf.sda_io_num = I2C_MASTER_SDA_IO;
-  conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-  conf.scl_io_num = I2C_MASTER_SCL_IO;
-  conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-  conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
-  i2c_param_config(i2c_master_port, &conf);
-  i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE,
-                     I2C_MASTER_TX_BUF_DISABLE, 0);
-  ets_printf("I2C initialised\n");
-}
+void i2c_master_init();
 
 #endif
