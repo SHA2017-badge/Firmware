c20e7886e09bc4b6edeae9bd66b711e6d5e1311c
Bas van Sisseren <bas@vansisseren.nl>
2017-05-23 22:45:21 +0200

    remove drawText() -- it has been replaced by draw_font()

--- components/epd/gde-driver.h
+++ components/epd/gde-driver.h
@@ -20,14 +20,6 @@ extern void writeLUT(int lut_idx);
 
 extern void drawImage(const unsigned char *picture);
 
-#define FONT_16PX 0x01
-#define FONT_INVERT 0x02
-#define FONT_FULL_WIDTH 0x04
-#define FONT_MONOSPACE 0x08
-#define FONT_UNDERLINE_1 0x10
-#define FONT_UNDERLINE_2 0x20
-extern int drawText(int x, int y, int y_len, const char *text, uint8_t flags);
-
 extern void setRamArea(unsigned char Xstart, unsigned char Xend,
                        unsigned short Ystart, unsigned short Yend);
 extern void setRamPointer(unsigned char addrX, unsigned short addrY);
