e086d6f41eb16d9145a120e514ef24bf7c9cc966
Niek Blankers <niek@niekproductions.com>
2017-06-01 17:21:35 +0200

    Nicer C uGFX demo

--- main/demo_ugfx.c
+++ main/demo_ugfx.c
@@ -5,17 +5,35 @@ font_t roboto;
 font_t robotoBlackItalic;
 font_t permanentMarker;
 
-void showDemo(color_t front, color_t back) {
+const char* displayNames[] = {
+    "Kartoffel",
+    "Sebastius",
+    "Boekenwuurm",
+    "Attilla",
+    "Stitch",
+    "tsd",
+    "Sprite_TM",
+    "Underhand",
+    "MarkusBec",
+    "Roosted",
+    "the_JinX",
+    "realitygaps",
+    "raboof"
+};
+
+#define numNames (sizeof(displayNames) / sizeof (const char*))
+
+void showDemo(uint8_t name, color_t front, color_t back) {
   gdispClear(back);
   gdispDrawString(150, 25, "STILL", robotoBlackItalic, front);
-  gdispDrawString(130, 50, "HACKING", permanentMarker, front);
+  gdispDrawString(130, 50, displayNames[name], permanentMarker, front);
   // underline:
   gdispDrawLine(127 + 3, 50 + 22,
-                127 + 3 + gdispGetStringWidth("HACKING", permanentMarker) + 14, 50 + 22,
+                127 + 3 + gdispGetStringWidth(displayNames[name], permanentMarker) + 14, 50 + 22,
                 front);
   // cursor:
-  gdispDrawLine(127 + 3 + gdispGetStringWidth("HACKING", permanentMarker) + 10, 50 + 2,
-                127 + 3 + gdispGetStringWidth("HACKING", permanentMarker) + 10, 50 + 22 - 2,
+  gdispDrawLine(127 + 3 + gdispGetStringWidth(displayNames[name], permanentMarker) + 10, 50 + 2,
+                127 + 3 + gdispGetStringWidth(displayNames[name], permanentMarker) + 10, 50 + 22 - 2,
                 front);
   gdispDrawString(140, 75, "Anyway", robotoBlackItalic, front);
   gdispDrawCircle(60, 60, 50, front);
@@ -31,13 +49,19 @@ void demoUgfx() {
   robotoBlackItalic = gdispOpenFont("Roboto_BlackItalic24");
   permanentMarker = gdispOpenFont("PermanentMarker22");
 
-  while (1) {
-    ets_printf("Make it White\n");
-    showDemo(Black, White);
-    ets_delay_us(5000000);
+  gdispClear(Black);
+  gdispFlush();
 
-    ets_printf("Paint it Black\n");
-    showDemo(White, Black);
-    ets_delay_us(5000000);
+  while (1) {
+    uint8_t i;
+    for (i = 0; i < numNames; i++) {
+      ets_printf("Make it White\n");
+      showDemo(i, Black, White);
+      ets_delay_us(5000000);
+
+      ets_printf("Paint it Black\n");
+      showDemo(i, White, Black);
+      ets_delay_us(5000000);
+    }
   }
 }
