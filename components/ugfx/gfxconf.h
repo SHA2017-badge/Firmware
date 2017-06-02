459e5cf287a3aec0654bc5d3b1f60896fd41f406
Anne Jan Brouwer <brouwer@annejan.com>
2017-06-02 22:23:22 +0200

    badge.text is now badge.string, added string_box, rounded_box and fill_rounded_box . . added UTF8 and Wordwrap to uGFX config

--- components/ugfx/gfxconf.h
+++ components/ugfx/gfxconf.h
@@ -85,11 +85,11 @@
 //#define GDISP_NEED_MULTITHREAD                       FALSE
 //#define GDISP_NEED_STREAMING                         FALSE
 #define GDISP_NEED_TEXT                              TRUE
-//    #define GDISP_NEED_TEXT_WORDWRAP                 FALSE
+#define GDISP_NEED_TEXT_WORDWRAP                     TRUE
 //    #define GDISP_NEED_TEXT_BOXPADLR                 1
 //    #define GDISP_NEED_TEXT_BOXPADTB                 1
 //    #define GDISP_NEED_ANTIALIAS                     FALSE
-//    #define GDISP_NEED_UTF8                          FALSE
+#define GDISP_NEED_UTF8                              TRUE
 //    #define GDISP_NEED_TEXT_KERNING                  FALSE
 //    #define GDISP_INCLUDE_FONT_UI1                   FALSE
 //    #define GDISP_INCLUDE_FONT_UI2                   FALSE		// The smallest preferred font.
