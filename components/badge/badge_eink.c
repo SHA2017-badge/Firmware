f7c2b71729cc2b2981d2b6bbb092d84649f3a5dc
Bas van Sisseren <bas@vansisseren.nl>
2017-06-08 00:21:41 +0200

    move initialize-code to badge_eink.c

--- components/badge/badge_eink.c
+++ components/badge/badge_eink.c
@@ -284,6 +284,63 @@ badge_eink_init(void)
 	// initialize spi interface to display
 	gdeInit();
 
-	// initialize display
-	initDisplay();
+#ifdef CONFIG_SHA_BADGE_EINK_GDEH029A1
+	/* initialize GDEH029A1 */
+
+	gdeReset();
+
+	// 0C: booster soft start control
+	gdeWriteCommand_p3(0x0c, 0xd7, 0xd6, 0x9d);
+
+	// 2C: write VCOM register
+	gdeWriteCommand_p1(0x2c, 0xa8); // VCOM 7c
+
+	// 11: data entry mode setting
+	gdeWriteCommand_p1(0x11, 0x03); // X inc, Y inc
+#endif // CONFIG_SHA_BADGE_EINK_GDEH029A1
+
+#ifdef CONFIG_SHA_BADGE_EINK_DEPG0290B1
+	/* initialize DEPG0290B01 */
+
+	// Hardware reset
+	gdeReset();
+	gdeBusyWait();
+
+	// Software reset
+	gdeWriteCommand(0x12);
+	gdeBusyWait();
+
+	// Set analog block control
+	gdeWriteCommand_p1(0x74, 0x54);
+
+	// Set digital block control
+	gdeWriteCommand_p1(0x7E, 0x3B);
+
+	// Set display size and driver output control
+	gdeWriteCommand_p3(0x01, 0x27, 0x01, 0x00);
+
+	// Ram data entry mode
+	// Adress counter is updated in Y direction, Y increment, X increment
+	gdeWriteCommand_p1(0x11, 0x03);
+
+	// Set RAM X address (00h to 0Fh)
+	gdeWriteCommand_p2(0x44, 0x00, 0x0F);
+
+	// Set RAM Y address (0127h to 0000h)
+	gdeWriteCommand_p4(0x45, 0x00, 0x00, 0x27, 0x01);
+
+	// Set border waveform for VBD (see datasheet)
+	gdeWriteCommand_p1(0x3C, 0x01);
+
+	// SET VOLTAGE
+
+	// Set VCOM value
+	gdeWriteCommand_p1(0x2C, 0x26);
+
+	// Gate voltage setting (17h = 20 Volt, ranges from 10v to 21v)
+	gdeWriteCommand_p1(0x03, 0x17);
+
+	// Source voltage setting (15volt, 0 volt and -15 volt)
+	gdeWriteCommand_p3(0x04, 0x41, 0x00, 0x32);
+#endif // CONFIG_SHA_BADGE_EINK_DEPG0290B1
 }
