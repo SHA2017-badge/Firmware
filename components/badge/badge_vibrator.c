ff1b5e86a62018c9d4b5d45206eae0250de8abfa
Bas van Sisseren <bas@vansisseren.nl>
2017-06-11 21:53:57 +0200

    added sdcard and vibrator modules; improved greyscale on depg display.

---
+++ components/badge/badge_vibrator.c
@@ -0,0 +1,70 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include "sdkconfig.h"
+
+#include "rom/ets_sys.h"
+#include "driver/spi_master.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "badge_pins.h"
+#include "badge_i2c.h"
+#include "badge_portexp.h"
+#include "badge_mpr121.h"
+#include "badge_vibrator.h"
+
+#if defined(PORTEXP_PIN_NUM_VIBRATOR) || defined(MPR121_PIN_NUM_VIBRATOR)
+
+static int
+badge_vibrator_on(void)
+{
+#ifdef PORTEXP_PIN_NUM_VIBRATOR
+	return badge_portexp_set_output_state(PORTEXP_PIN_NUM_VIBRATOR, 1);
+#elif defined(MPR121_PIN_NUM_VIBRATOR)
+	return badge_mpr121_set_gpio_level(MPR121_PIN_NUM_VIBRATOR, 1);
+#endif
+}
+
+static int
+badge_vibrator_off(void)
+{
+#ifdef PORTEXP_PIN_NUM_VIBRATOR
+	return badge_portexp_set_output_state(PORTEXP_PIN_NUM_VIBRATOR, 0);
+#elif defined(MPR121_PIN_NUM_VIBRATOR)
+	return badge_mpr121_set_gpio_level(MPR121_PIN_NUM_VIBRATOR, 0);
+#endif
+}
+
+void
+badge_vibrator_activate(int pattern)
+{
+	while (pattern != 0)
+	{
+		if ((pattern & 1) == 0)
+			badge_vibrator_off();
+		else
+			badge_vibrator_on();
+		pattern >>= 1;
+		ets_delay_us(200000);
+	}
+	badge_vibrator_off();
+}
+
+void
+badge_vibrator_init(void)
+{
+	// configure vibrator pin
+#ifdef PORTEXP_PIN_NUM_VIBRATOR
+	badge_portexp_set_output_state(PORTEXP_PIN_NUM_VIBRATOR, 0);
+	badge_portexp_set_output_high_z(PORTEXP_PIN_NUM_VIBRATOR, 0);
+	badge_portexp_set_io_direction(PORTEXP_PIN_NUM_VIBRATOR, 1);
+#elif defined(MPR121_PIN_NUM_VIBRATOR)
+	badge_mpr121_configure_gpio(MPR121_PIN_NUM_VIBRATOR, MPR121_OUTPUT);
+#endif
+}
+
+#endif // defined(PORTEXP_PIN_NUM_VIBRATOR) || defined(MPR121_PIN_NUM_VIBRATOR)
---
+++ components/badge/badge_vibrator.h
@@ -0,0 +1,7 @@
+#ifndef BADGE_VIBRATOR_H
+#define BADGE_VIBRATOR_H
+
+extern void badge_vibrator_init(void);
+extern void badge_vibrator_activate(int pattern);
+
+#endif // BADGE_VIBRATOR_H
