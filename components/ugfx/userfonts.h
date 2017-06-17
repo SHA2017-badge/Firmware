67dfe611762696015035cd7c5bc6af27d4aadeca
doebi <christoph@doeberl.at>
2017-06-17 19:37:41 +0200

    added fonts for use in the weather app

--- components/ugfx/userfonts.h
+++ components/ugfx/userfonts.h
@@ -4,11 +4,13 @@
  * To include more fonts convert them from ttf using
  * http://fonts.ugfx.io/ and include them below.
  */
-#include "Roboto-Black22.c"
-#include "Roboto-BlackItalic24.c"
-#include "Roboto-Regular12.c"
-#include "Roboto-Regular18.c"
-#include "Roboto-Regular22.c"
-#include "PermanentMarker22.c"
-#include "PermanentMarker36.c"
-
+#include "fonts/Roboto-Black22.c"
+#include "fonts/Roboto-BlackItalic24.c"
+#include "fonts/Roboto-Regular12.c"
+#include "fonts/Roboto-Regular18.c"
+#include "fonts/Roboto-Regular22.c"
+#include "fonts/PermanentMarker22.c"
+#include "fonts/PermanentMarker36.c"
+#include "fonts/DejaVuSans20.c"
+#include "fonts/weather42.c"
+#include "fonts/pixelade13.c"
