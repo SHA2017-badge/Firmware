0b095cd537967a014c04f55886de9f6e7fd8d04b
Bas van Sisseren <bas@vansisseren.nl>
2017-06-24 13:39:38 +0200

    use doxygen documentation style

--- components/badge/badge_power.h
+++ components/badge/badge_power.h
@@ -1,15 +1,33 @@
+/** @file badge_power.h */
 #ifndef BADGE_POWER_H
 #define BADGE_POWER_H
 
 #include <stdbool.h>
 
+/**
+ * initializes the battery and usb power sensing
+ */
 extern void badge_power_init(void);
+
+/**
+ * returns the charging state.
+ *
+ * @return true if charging; false otherwise.
+ */
 extern bool badge_battery_charge_status(void);
 
-// returns Vbat voltage in mV
+/**
+ * returns the Vbat voltage.
+ *
+ * @return Vbat voltage in mV; -1 on error
+ */
 extern int badge_battery_volt_sense(void);
 
-// returns Vusb voltage in mV
+/**
+ * returns the Vusb voltage.
+ *
+ * @return Vusb voltage in mV; -1 on error
+ */
 extern int badge_usb_volt_sense(void);
 
 #endif // BADGE_POWER_H
