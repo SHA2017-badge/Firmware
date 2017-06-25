0492bdd4376a9d793e1582f23fe7ef5ee177f173
Anne Jan Brouwer <brouwer@annejan.com>
2017-06-25 16:53:17 +0200

    Sponsor images to make Projectleitung happy . .

---
+++ main/leaseweb.h
@@ -0,0 +1,8 @@
+#ifndef LEASEWEB_H
+#define LEASEWEB_H
+
+#include <stdint.h>
+
+const uint8_t leaseweb[4736];
+
+#endif // LEASEWEB_H
---
+++ main/madison_gurkha.h
@@ -0,0 +1,8 @@
+#ifndef MG_LOGO_H
+#define MG_LOGO_H
+
+#include <stdint.h>
+
+const uint8_t mg_logo[4736];
+
+#endif // MG_LOGO_H
