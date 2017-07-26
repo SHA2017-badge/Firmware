cfde4bfaf21d98bedfe9068bb796ab1b58b1445a
Anne Jan Brouwer <brouwer@annejan.com>
2017-07-26 12:06:08 +0200

    Keeping all up to date and moved comment around a bit

--- components/bpp-if/bpp_init.c
+++ components/bpp-if/bpp_init.c
@@ -64,9 +64,9 @@ void doDeepSleep(int delayMs, void *arg) {
 	if (delayMs<5000) return; //not worth sleeping
 	printf("Sleeping for %d ms...\n", delayMs);
 	blockdecodeShutDown(otablockdecoder);
-  // FIXME the wake on touch should be in badge_input_init
+  // TODO the wake on touch should be in badge_input_init
   esp_deep_sleep_enable_ext0_wakeup(GPIO_NUM_25 , 0);
-  // TODO don't use hardcoded GPIO_NUM_25
+  // FIXME don't use hardcoded GPIO_NUM_25
 	esp_deep_sleep_enable_timer_wakeup(delayMs*1000);
 	esp_deep_sleep_start();
 }
